3ce6eef0313b1325985271034022212b379698c5
kieran Sweetman <kikisweetman@gmail.com>
2022-04-27 14:38:26 +0200

    adding files

---
+++ portfolio/scripts/script.js
@@ -0,0 +1,13 @@
+console.log('loaded')
+const expand = document.getElementById('expand');
+const card_cont = document.getElementById('card_cont');
+function personInfo(info, site){
+    const aTag = document.createElement('a');
+
+}
+
+expand.addEventListener('click', () => {
+    console.log('it worked')
+    card_cont.style.height ="60%"
+    
+})
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["D:/coding/pf2/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
