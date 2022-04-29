ed6323b6ac05f36ebe13d58c5d4f5f5c81899c0f
kieran Sweetman <kikisweetman@gmail.com>
2022-04-29 16:18:46 +0200

    fixed basic formatting and added page scroller

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -1,26 +1,75 @@
+
 console.log('loaded')
+
+// contact card info
 const expand = document.getElementById('expand');
 const card_cont = document.getElementById('card_cont');
 const more_cont = document.getElementById('more_cont');
 
-function personInfo(info, site){
-    const aTag = document.createElement('a');
-
-}
-
 expand.addEventListener('click', () => {
 
-   const src = expand.getAttribute('name');
+    const src = expand.getAttribute('name');
+ 
+    if (src === 'down') {
+        expandDown();
+    } else {
+        expandUp();
+    }
+ 
+     
+     
+ })
 
-   if (src === 'down') {
-       expandDown();
-   } else {
-       expandUp();
-   }
+// tab down buttons
 
-    
+const tabButtons = document.querySelectorAll('#tab')
+const content = document.querySelectorAll('#content');
+tabButtons.forEach(input => {
+    input.addEventListener('click', () => {
+        let name = name_extractor(input);
+
+        if (name === 4){
+            content[0].scrollIntoView({behavior:'smooth', block:'end'})
+        
+        }
+
+        name += 1;
+        console.log(content);
+
+
+
+        content.forEach(element => {
+            let result = name_extractor(element);
+            if (result === name) {
+                element.scrollIntoView({behavior: 'smooth', block:'end'})
+            }
+        })
+
+    })
     
 })
+
+function name_extractor(element) {
+    return Number(element.getAttribute('name'));
+}
+
+
+const sec1 = document.getElementById('sec1');
+const sec2 = document.getElementById('sec2');
+const sec3 = document.getElementById('sec3');
+
+const second = document.getElementById('second');
+// const third = document.querySelector('3')
+
+
+sec1.addEventListener('click', () => {
+    second.scrollIntoView({behavior: "smooth", block: 'end',})
+})
+sec2.addEventListener('click', () => {
+    third.scrollIntoView({behavior: "smooth", block: 'end',})
+})
+
+
 
 function expandDown() {
     
