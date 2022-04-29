57c29dbbff60beb1d26f3aedc66bd7671e5d9a20
kieran Sweetman <kikisweetman@gmail.com>
2022-04-29 16:25:28 +0200

    cleaning js script

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -20,6 +20,34 @@ expand.addEventListener('click', () => {
      
  })
 
+
+
+ function expandDown() {
+    
+    card_cont.style.transition = "height .2s ease-in-out";
+    card_cont.style.height ="80%";
+    
+    more_cont.style.transition = "opacity .3s ease-in-out .2s"
+    more_cont.style.opacity = "1";
+    expand.src = "./portfolio/images/svg/chevron-up.svg";
+    expand.setAttribute('name', 'up')
+
+}
+
+
+function expandUp(){
+
+    card_cont.style.transition = "height .2s ease-in-out";
+    card_cont.style.height ="60%";
+
+    more_cont.style.transition = "opacity .1s ease-in-out"
+    more_cont.style.opacity = "0";
+
+    expand.src = "./portfolio/images/svg/chevron-down.svg";
+    expand.setAttribute('name', 'down');
+    
+}
+
 // tab down buttons
 
 const tabButtons = document.querySelectorAll('#tab')
@@ -54,45 +82,6 @@ function name_extractor(element) {
 }
 
 
-const sec1 = document.getElementById('sec1');
-const sec2 = document.getElementById('sec2');
-const sec3 = document.getElementById('sec3');
-
-const second = document.getElementById('second');
-// const third = document.querySelector('3')
-
-
-sec1.addEventListener('click', () => {
-    second.scrollIntoView({behavior: "smooth", block: 'end',})
-})
-sec2.addEventListener('click', () => {
-    third.scrollIntoView({behavior: "smooth", block: 'end',})
-})
 
 
 
-function expandDown() {
-    
-    card_cont.style.transition = "height .2s ease-in-out";
-    card_cont.style.height ="80%";
-    
-    more_cont.style.transition = "opacity .3s ease-in-out .2s"
-    more_cont.style.opacity = "1";
-    expand.src = "./portfolio/images/svg/chevron-up.svg";
-    expand.setAttribute('name', 'up')
-
-}
-
-
-function expandUp(){
-
-    card_cont.style.transition = "height .2s ease-in-out";
-    card_cont.style.height ="60%";
-
-    more_cont.style.transition = "opacity .1s ease-in-out"
-    more_cont.style.opacity = "0";
-
-    expand.src = "./portfolio/images/svg/chevron-down.svg";
-    expand.setAttribute('name', 'down');
-    
-}
